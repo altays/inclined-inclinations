374c88f8222b73f556f6dc90b0a1af272b0a8f1c
Alex Taylor <altays633@gmail.com>
2021-10-17 15:42:55 -0400

    adding test RiGrammar script from documentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
 window.onload = function() {
 
-    let main = document.getElementsByTagName("main")[0]
+    // let main = document.getElementsByTagName("main")[0]
     let contentDiv = document.getElementById("content")
     let genButton = document.getElementsByClassName("button")[0]
     let promptPara = document.getElementById("prompt")
-    let introParas = document.getElementsByClassName('intro')
-
-    let testCount = "test prompt of a set amount of characters. maybe two stencnes. perhaps three? who knows! Try to code something else because this isn't working";
+    // let introParas = document.getElementsByClassName('intro')
 
     let textParaPrompt = document.createElement('p');
     textParaPrompt.setAttribute("id","prompt")
@@ -14,9 +12,19 @@ window.onload = function() {
     
     genButton.addEventListener('click', updatePrompt);
 
+    let rules = new Object(    {
+        start: "$subject $verb $object.",
+        subject: "I | You | They",
+        object: "coffee | bread | milk",
+        verb: "want | hate | like | love"
+    }) 
+    
+    let rg = RiTa.grammar(rules);  // load our grammar
+
     function updatePrompt(){
+        let result = rg.expand();      // and then run it!
         if (promptPara === null || promptPara.textContent === null) {
-            textParaPrompt.textContent = testCount;
+            textParaPrompt.textContent = result;
             contentDiv.appendChild(textParaPrompt)
         }  
 
